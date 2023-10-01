d6311f440e4e608ec7f91a6a6bc28a0c5ffbe027
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2023-10-01 13:44:21 +0530

    Create client.c

---
+++ client.c
@@ -0,0 +1,35 @@
+// client.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    int sockfd;
+    struct sockaddr_in server_addr;
+    char message[100];
+
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Error in socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);                   // Choose any available port for communication
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // localhost
+
+    // Connect to server
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error in connect");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
